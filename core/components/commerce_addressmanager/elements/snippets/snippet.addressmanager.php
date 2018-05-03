088f6963017cb0f4c1ae799249434c51886d37df
Tony <tonyklapatch@hotmail.com>
2018-05-02 21:25:54 -0400

    Add AddressManager class and basic get addresses toArray for testing

--- core/components/commerce_addressmanager/elements/snippets/snippet.addressmanager.php
+++ core/components/commerce_addressmanager/elements/snippets/snippet.addressmanager.php
@@ -2,6 +2,26 @@
 /**
  * AddressManager for Modmore's Commerce
  * 
- * Made by Tony Klapatch
+ * Made by Tony Klapatch <tony@klapatch.net>
  * https://github.com/poconosewandvac/Commerce_AddressManager
  */
+
+// Check if user is logged in
+$user = $modx->user->get('id');
+if (!$user) {
+    $modx->sendUnauthorizedPage();
+}
+
+// Load AddressManager class
+$addressMgr = $modx->getService('addressmanager','AddressManager', $modx->getOption('commerce_addressmanager.core_path', null, $modx->getOption('core_path').'components/commerce_addressmanager/').'model/commerce_addressmanager/', $scriptProperties);
+if (!($addressMgr instanceof AddressManager) && !($addressMgr->commerce instanceof Commerce)) return '';
+if ($addressMgr->commerce->isDisabled()) {
+    return $modx->lexicon('commerce.mode.disabled.message');
+}
+
+// Load user's addresses
+$addresses = $addressMgr->getAddresses($user);
+
+foreach ($addresses as $a) {
+    print_r($a->toArray());
+}
---
+++ core/components/commerce_addressmanager/model/commerce_addressmanager/addressmanager.class.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * AddressManager for Modmore's Commerce
+ * 
+ * Made by Tony Klapatch <tony@klapatch.net>
+ * https://github.com/poconosewandvac/Commerce_AddressManager
+ */
+
+class AddressManager {
+    public $modx;
+    public $commerce;
+    public $config = [];
+
+    public function __construct(modX &$modx, array $config = array()) {
+        // Initialize AddressManager
+        $this->modx =& $modx;
+        $basePath = $this->modx->getOption('commerce_addressmanager.core_path', $config, $this->modx->getOption('core_path').'components/commerce_addressmanager/');
+        $assetsUrl = $this->modx->getOption('commerce_addressmanager.assets_url', $config, $this->modx->getOption('assets_url').'components/commerce_addressmanager/');
+
+        $this->config = array_merge([
+            'corePath' => $corePath,
+            'modelPath' => $corePath.'model/',
+            'processorsPath' => $corePath.'processors/',
+            'controllersPath' => $corePath.'controllers/',
+            'chunksPath' => $corePath.'elements/chunks/',
+            'snippetsPath' => $corePath.'elements/snippets/',
+            'baseUrl' => $assetsUrl,
+            'cssUrl' => $assetsUrl.'css/',
+            'jsUrl' => $assetsUrl.'js/',
+            'connectorUrl' => $assetsUrl.'connector.php'
+        ]);
+
+        // Load Commerce
+        $commercePath = $this->modx->getOption('commerce.core_path', null, $this->modx->getOption('core_path') . 'components/commerce/') . 'model/commerce/';
+        $commerceParams = ['mode' => $this->modx->getOption('commerce.mode')];
+        $this->commerce = $this->modx->getService('commerce', 'Commerce', $commercePath, $commerceParams);
+    }
+
+    /**
+     * Gets the user's addresses from comAddress.
+     * 
+     * @param int $user User ID to reference the address
+     * @return comAddress xPDOObjects collection
+     */
+    public function getAddresses($user) {
+        $query = $this->modx->newQuery('comAddress');
+        $query->where([
+            'user' => $user,
+            'remember' => 1
+        ]);
+        return $this->modx->getCollection('comAddress', $query);
+    }
+}
