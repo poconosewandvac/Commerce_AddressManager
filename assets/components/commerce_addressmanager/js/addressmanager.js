c66e4ac03c8765618a90660cd4bdc4a494e8d08b
Tony Klapatch <tonyklapatch@hotmail.com>
2019-01-18 21:37:23 -0500

    Frontend css & js improvements

--- assets/components/commerce_addressmanager/js/addressmanager.js
+++ assets/components/commerce_addressmanager/js/addressmanager.js
@@ -1,21 +1,35 @@
-var addresses = document.querySelectorAll(".address-open");
-var deleteButtons = document.querySelectorAll(".address-button-delete");
-
-for (var i = 0; i < addresses.length; i++) {
-    addresses[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-
-        var addressPane = this.nextElementSibling;
+(function() {
+    var addresses = document.querySelectorAll(".address-open");
+    var deleteButtons = document.querySelectorAll(".address-button-delete");
+    var activeAddress = document.querySelector(".address-open.active");
+    
+    function openAddress(target) {
+        var addressPane = target.nextElementSibling;
         if (addressPane.style.maxHeight) {
             addressPane.style.maxHeight = null;
         } else {
             addressPane.style.maxHeight = addressPane.scrollHeight + "px";
         }
-    });
-}
-
-for (var i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].addEventListener("click", function (e) {
-        if (!confirm("Are you sure you want to delete this address?")) e.preventDefault();
-    });
-}
+    }
+    
+    // Set active address to expand and scroll to it (address with error)
+    if (activeAddress) {
+        openAddress(activeAddress);
+        activeAddress.scrollIntoView();
+    }
+    
+    // Add the event listener for expando
+    for (var i = 0; i < addresses.length; i++) {
+        addresses[i].addEventListener("click", function (e) {
+            this.classList.toggle("active");
+            openAddress(e.target);
+        });
+    }
+    
+    // Add delete prompt to delete buttons
+    for (var i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener("click", function (e) {
+            if (!confirm("Are you sure you want to delete this address?")) e.preventDefault();
+        });
+    }
+})();
